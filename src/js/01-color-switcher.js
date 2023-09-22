a7392b3fbad968178fc2cbba6bf8334ac6d3a101
Igor Liakh <igorlyah060@gmail.com>
2023-09-22 18:19:17 +0300

    done task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,32 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
 
+let idInterval;
+
+const startHandler = e => {
+  e.target.disabled = true;
+  stopBtn.disabled = false;
+  idInterval = setInterval(() => {
+    const bgColor = getRandomHexColor();
+    body.style.background = bgColor;
+  }, 1000);
+
+  //   return idInterval;
+};
+
+const stopHandler = e => {
+  e.target.disabled = true;
+  startBtn.disabled = false;
+  clearInterval(idInterval);
+};
+
+startBtn.addEventListener('click', startHandler);
+
+stopBtn.addEventListener('click', stopHandler);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
